90f79bbfa7abb4d3e326e6a7e11fc00da0da579a
Akashi, Jon-Erik | Jon | INSUB <jon-erik.akashi@rakuten.com>
2017-10-20 17:23:02 +0900

    adding file

---
+++ reddit.py
@@ -0,0 +1,22 @@
+import urllib
+import json
+from collections import defaultdict
+
+serviceurl = 'https://www.reddit.com/r/'
+
+while True:
+    subreddit = raw_input('Enter subreddit: ')
+    if len(subreddit) < 1 : subreddit = 'music';
+    if subreddit == 'end': break
+
+    url = serviceurl + subreddit + '.json'
+    print 'Retrieving /r/' + subreddit.capitalize()
+    uh = urllib.urlopen(url)
+    data = uh.read()
+    print 'Retrieved', len(data), 'characters'
+
+    try: js = json.loads(str(data))
+    except: js = None
+
+    plug = js['data']['children']['data'][0]['domain']
+    print plug
