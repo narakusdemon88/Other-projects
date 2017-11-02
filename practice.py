e8b7e0dc06ea02b4f9704ba688fbf30fac056759
narakusdemon88 <narakusdemon88@gmail.com>
2017-11-02 18:57:30 +0900

    Delete practice.py

--- practice.py
+++
@@ -1,9 +0,0 @@
-import requests
-import json
-
-r = requests.get('https://www.reddit.com/r/worldnews.json', headers = {'User-agent': 'Chrome'})
-
-theJSON = json.loads(r.text)
-
-for items in theJSON:
-	print items
