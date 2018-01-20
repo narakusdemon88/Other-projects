45be5dbe0538244305c3bbd40b8d6d991c1321a1
Akashi <jon-erik.akashi@rakuten.com>
2018-01-20 20:02:25 +0900

    uploading beta of download.py

--- wallpaper.py
+++ wallpaper.py
@@ -1,18 +1,21 @@
 import urllib
+import urllib2
 import json 
 from collections import defaultdict
 
 serviceurl = 'https://www.reddit.com'
 
 while True:
-    subreddit = input('Enter subreddit: ')
+    subreddit = raw_input('Enter subreddit: ')
     if len(subreddit) < 1 : subreddit = 'wallpapers';
+    if subreddit == 'anime':
+        url = 'https://www.reddit.com/r/Animewallpaper/top/.json?sort=top'
     if subreddit == 'end': break
 
     if subreddit != 'mine': 
         url = serviceurl + '/r/' + subreddit + '/top/.json?sort=top'
-        print ('Retrieving /r/' + subreddit.capitalize())
-    uh = urllib.request.urlopen(url)
+        print 'Retrieving /r/' + subreddit.capitalize() + ' from ' + url
+    uh = urllib.urlopen(url)
     data = uh.read()
 
     try: js = json.loads(str(data))
@@ -23,13 +26,13 @@ while True:
         if items['data']['domain'] == 'i.imgur.com' :
             imgurUrl = items['data']['url']
             imgurTitle = items['data']['title']
+            imgurFileType = imgurUrl[-4:]
 
-            print (imgurTitle, imgurUrl)
+            def downloadWebImage(url):
+                request = urllib2.Request(url)
+                img = urllib2.urlopen(request).read()
+                with open (imgurTitle + imgurFileType, 'w') as f: f.write(img)
 
+            downloadWebImage(imgurUrl)
 
-import urllib.request
-
-print('Beginning file download with urllib2...')
-
-url = 'http://i3.ytimg.com/vi/J---aiyznGQ/mqdefault.jpg'
-urllib.request.urlretrieve(url, '/Users/jon-erik.akashi/Desktop/test folder/cat.jpg')
+            print imgurTitle, imgurUrl, imgurFileType
